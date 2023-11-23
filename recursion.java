ba360da92b2144a8fbd2606b5f0a97975fa95e7b
unknown <2023415sreng@aupp.edu.kh>
2023-11-23 10:35:15 +0700

    M

--- recursion.java
+++ recursion.java
@@ -1,15 +1,22 @@
 public class recursion {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        recursionFunction(10);
+        
 
-    //call your recursion fuction
-    //print result
 
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+        //call your recursion fuction
+        //print result
+
     }
-  
+
+    public static void recursionFunction(int n) {
+
+        if (n <= 0) {
+            System.out.println("Countdown complete!");
+        } else {
+            System.out.println(n);
+            recursionFunction(n - 1);
+        }
+
+    }
 }
-    
